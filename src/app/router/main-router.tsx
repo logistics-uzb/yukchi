52877e2dfb195944a940436f7ca753702f52cc55
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2025-10-28 16:29:58 +0500

    feat: Implement font "Outfit"

--- src/app/router/main-router.tsx
+++ src/app/router/main-router.tsx
@@ -1,3 +1,4 @@
+import { LoginPage } from "@pages/login";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -5,4 +6,8 @@ export const router = createBrowserRouter([
     path: "/",
     element: <div>Hello World</div>,
   },
+  {
+    path: "/login",
+    element: <LoginPage />
+  }
 ]);
